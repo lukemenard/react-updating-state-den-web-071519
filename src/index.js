23c6173b5601bf6055ee387ab0c7d230dcc3550f
Luke Menard <lukemenard@gmail.com>
2020-12-28 21:46:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom';
 // import ClickityClick from './components/ClickityClick'
 
 ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
-  document.getElementById('root')
+  <ClickityClick />, document.getElementById('root')
 );
